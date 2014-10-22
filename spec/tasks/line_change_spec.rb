33d65f237e06617a99683be08c7748ade8fd7233
Yuki Nishijima <pair+ynishijima@pivotal.io>
2014-10-22 15:48:54 -0400

    Make sure line_change/tasks defines tasks as expected

--- spec/tasks/line_change_spec.rb
+++ spec/tasks/line_change_spec.rb
@@ -16,4 +16,32 @@ describe 'hockeyapp task' do
       end
     end
   end
+
+  context 'config with path', if: LineChange.config_path.end_with?('config_with_path.yml') do
+    describe 'staging' do
+      let(:app_id) { 'staging_id' }
+      let(:apk_path) { 'path/to/uploadable/staging/file' }
+
+      it 'uploads an apk to production' do
+        allow(LineChange).to receive(:deploy)
+
+        Rake::Task['line_change:staging'].invoke
+
+        expect(LineChange).to have_received(:deploy).with(app_id, apk_path)
+      end
+    end
+
+    describe 'production' do
+      let(:app_id) { 'production_id' }
+      let(:apk_path) { 'path/to/uploadable/production/file' }
+
+      it 'uploads an apk to production' do
+        allow(LineChange).to receive(:deploy)
+
+        Rake::Task['line_change:production'].invoke
+
+        expect(LineChange).to have_received(:deploy).with(app_id, apk_path)
+      end
+    end
+  end
 end
