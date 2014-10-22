9bd317b9223c30936d9a44d0151ec910c9ca395d
Yuki Nishijima <pair+ynishijima@pivotal.io>
2014-10-22 15:43:55 -0400

    Output progress and result while deploying

--- lib/line_change/deploy.rb
+++ lib/line_change/deploy.rb
@@ -2,12 +2,23 @@ require 'line_change/connection'
 
 module LineChange
   class Deploy
+    attr_reader :id, :path
+
     def initialize(id, path)
       @id, @path = id, path
     end
 
     def start
-      connection.upload(@path, @id)
+      print "Uploading #{path} to Hockeyapp... "
+
+      connection.upload(path, id).tap do |response|
+        puts "Done!" "\n\n"
+        puts "Response from Hockeyapp:"
+
+        response.body.each do |key, value|
+          puts "#{' ' * 4}" "#{key.ljust(19)}: #{value}"
+        end
+      end
     end
 
     private
--- spec/deploy_spec.rb
+++ spec/deploy_spec.rb
@@ -1,18 +1,41 @@
 describe LineChange::Deploy do
   describe '#start' do
-    let(:apk_path) { 'apk path' }
     let(:app_id) { 'app id' }
     let(:connection) { instance_double(LineChange::Connection) }
+    let(:response) { double(:response, body: {'great' => 'result!'}) }
 
     subject { LineChange::Deploy.new(app_id, apk_path) }
 
-    it "uploads the path to the connection" do
+    before do
       allow(LineChange::Connection).to receive(:new).and_return(connection)
-      allow(connection).to receive(:upload)
-
-      subject.start
-
-      expect(connection).to have_received(:upload).with(apk_path, app_id)
+      allow(connection).to receive(:upload) { response }
     end
+
+    context "when file exists" do
+      let(:apk_path) { "spec/support/fixtures/newest.txt" }
+
+      it "uploads the path to the connection" do
+        without_output { subject.start }
+
+        expect(connection).to have_received(:upload).with(apk_path, app_id)
+      end
+
+      it "outputs progress and result" do
+        output =
+          "Uploading spec/support/fixtures/newest.txt to Hockeyapp... Done!\n" \
+          "\n" \
+          "Response from Hockeyapp:\n" \
+          "    great              : result!\n"
+
+        expect { subject.start }.to output(output).to_stdout
+      end
+    end
+  end
+
+  def without_output
+    org, $stdout = $stdout, double(:stdout, write: nil)
+    yield
+  ensure
+    $stdout = org
   end
 end
