2786f1550a87947466ef73bf5d91bfc897dc4e96
Yuki Nishijima <pair+ynishijima@pivotal.io>
2014-10-22 10:06:00 -0400

    Add description about the rake tasks defined by LineChange

--- lib/line_change/tasks.rb
+++ lib/line_change/tasks.rb
@@ -2,6 +2,7 @@ require 'rake'
 
 namespace :line_change do
   LineChange.configuration.apps.each do |env, app_id|
+    desc "Upload apk to #{env} (app_id: #{app_id})"
     task env.to_sym, :apk_path do |_, args|
       LineChange.deploy(app_id, args[:apk_path])
     end
