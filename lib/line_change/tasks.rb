06881fcf507c77c460da29c0d03af4ff387c3fce
Yuki Nishijima <pair+ynishijima@pivotal.io>
2014-10-22 09:40:58 -0400

    We no longer have to use api_key when creating tasks

--- lib/line_change/tasks.rb
+++ lib/line_change/tasks.rb
@@ -1,10 +1,7 @@
 require 'rake'
 
 namespace :line_change do
-  api_key = LineChange.configuration.api_key
-  apps = LineChange.configuration.apps
-
-  apps.each do |env, app_id|
+  LineChange.configuration.apps.each do |env, app_id|
     task env.to_sym, :apk_path do |_, args|
       LineChange.deploy(app_id, args[:apk_path])
     end
