d72039941c18d7e4cf683216daf5ba3bf7b87d4c
Yuki Nishijima <pair+ynishijima@pivotal.io>
2014-10-22 17:49:05 -0400

    Do not accept apk_path when path is already specified in config/line_change.yml

--- lib/line_change/tasks.rb
+++ lib/line_change/tasks.rb
@@ -2,9 +2,12 @@ require 'rake'
 
 namespace :line_change do
   LineChange.configuration.apps.each do |app|
-    desc "Upload apk to #{app.env} (app_id: #{app.app_id})"
-    task app.env, :apk_path do |_, args|
-      LineChange.deploy(app.app_id, args[:apk_path] || app.path)
+    desc "Uploads apk to #{app.env} (app_id: #{app.app_id})"
+
+    if app.path
+      task(app.env){ LineChange.deploy(app.app_id, app.path) }
+    else
+      task(app.env, :apk_path){|_, args| LineChange.deploy(app.app_id, args[:apk_path]) }
     end
   end if LineChange::Configuration.exists?
 
